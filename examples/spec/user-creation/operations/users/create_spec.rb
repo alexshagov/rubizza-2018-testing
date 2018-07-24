c0c65d077a11a0ebee79a58082e33e156ae5af57
Alexander Shagov <alexander.shgv@gmail.com>
2018-07-24 18:45:02 +0300

    Add tests structure for users create operation

--- examples/spec/user-creation/operations/users/create_spec.rb
+++ examples/spec/user-creation/operations/users/create_spec.rb
@@ -2,5 +2,23 @@ require 'spec_helper'
 require './user-creation/operations/users/create.rb'
 
 RSpec.describe Operations::Users::Create do
+  describe "#call" do
+    it 'creates user' do
 
+    end
+
+    it 'sends email to user\'s email' do
+
+    end
+
+    it 'sends notification to external API about user creation' do
+
+    end
+
+    context 'when user already exists' do
+      it 'returns appropriate error' do
+
+      end
+    end
+  end
 end
